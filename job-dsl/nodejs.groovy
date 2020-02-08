b350b45dda1348c9c4d72da73d6eb19d27b078d5
Raúl Pérez <48015395+mapevira@users.noreply.github.com>
2020-02-08 09:34:42 +0100

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
-job('NodeJS example') {
+job('NodeJS-example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('raulp2003pax@outlook.com')
         }
     }
     triggers {
